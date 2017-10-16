3a81d45b02f997aff66e0c263920f71155cd1ecb
TANT-A01\eagro02 <eagro02@ca.com>
2017-10-16 18:47:13 -0500

    Added distance method to World.

--- src/worlds/World.java
+++ src/worlds/World.java
@@ -3,6 +3,8 @@ package worlds;
 import java.util.HashMap;
 
 /**
+ * A plain old Data Object.
+ *
  * Created by eagro02 on 10/16/2017.
  */
 public class World
@@ -26,6 +28,27 @@ public class World
         remarks = hash.get("Remarks").toString();
         worldx  = hash.get( "WorldX" ).toString();
         worldy  = hash.get( "WorldY" ).toString();
+    }
+
+    public int distanceTo( World otherWorld )
+    {
+        int row1 = Integer.parseInt( worldy );
+        int row2 = Integer.parseInt( otherWorld.worldy );
+        int col1 = Integer.parseInt( worldx );
+        int col2 = Integer.parseInt( otherWorld.worldx );
+
+        int a1 = row1 + ( col1 / 2 );
+        int a2 = row2 + ( col2 / 2 );
+
+        int d1 = Math.abs( a1 - a2 );
+        int d2 = Math.abs( col1 - col2 );
+        int d3 = Math.abs( (a1 - col1) - (a2 - col2) );
+
+        // return the largest value from among d1, d2, d3.
+
+        if ( d1 > d2 && d2 > d3 ) return d1;
+        if ( d2 > d1 && d1 > d3 ) return d2;
+        return d3; // last man standing
     }
 
     public String toString()
